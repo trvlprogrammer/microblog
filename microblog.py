8882fc9ed4e721dcc5f44c3f4c5b3819fcf99a96
Alfatih Ridho NT <52704579+trvlprogrammer@users.noreply.github.com>
2022-06-15 16:02:44 +0700

    lesson :user login
    lesson :user login

--- microblog.py
+++ microblog.py
@@ -1,6 +1,11 @@
+from sqlalchemy import true
 from app import app, db
 from app.models import User, Post
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
+
+
+if __name__ == '__main__':
+    app.run(debug=true)
